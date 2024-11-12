cf650c3859534171a9ae67b95668ab47d6c5b6a4
Megh2005 <megh.deb.civil27@heritageit.edu.in>
2024-11-13 00:28:08 +0530

    adjust autoplay interval for animated testimonials from 3000ms to 5000ms

--- components/ui/animated-testimonials.jsx
+++ components/ui/animated-testimonials.jsx
@@ -24,7 +24,7 @@ export const AnimatedTestimonials = ({
 
   useEffect(() => {
     if (autoplay) {
-      const interval = setInterval(handleNext, 3000);
+      const interval = setInterval(handleNext, 5000);
       return () => clearInterval(interval);
     }
   }, [autoplay]);
